237bc3b823605c53ff64ba4c8697648a41053696
Nataly <natasha-vd@yandex.ru>
2021-04-13 23:36:35 +0300

    ДЗ-3 1 задача через флаг

--- homework3/task3.1.js
+++ homework3/task3.1.js
@@ -1,5 +1,44 @@
 //Решение задачи №1
 
+//Решение №1
+
+const MAX = 100;
+let number = 2;
+
+    while(number <= MAX) {
+        let isPrime = false;
+        
+        for(let j = 2; j < number; j++) {
+            if(number % j === 0) {
+                isPrime = true;
+                break;
+            }
+        };
+
+        if(isPrime === false) {
+            console.log(number);
+        };
+        
+        number++;
+    }
+
+/*Решение №2
+
+const MAX = 100;
+let number = 1;
+
+outer:
+    while(number < MAX) {
+        number++;
+        for(let j = 2; j < number; j++) {
+            if(number % j === 0) {
+                continue outer;
+            }
+        }
+        console.log(number);
+    }*/
+
+/*Решение №3
 const MAX = 100;
 
 outer:
@@ -10,9 +49,10 @@ for(let i = 2; i <= MAX; i++) {
         }
     }
     console.log(i);
-}
+}*/
 
-/*const MAX = 100;
+/*Решение №4
+const MAX = 100;
 let number = 2; // беру от 2-х, т.к. в простые числа 0 и 1 не входят, по определению простых чисел
 
 while(number <= MAX) {
@@ -43,7 +83,8 @@ while(number <= MAX) {
     number++;
 }*/
 
-/* Можно простые числа накапливать в массиве
+/* Решение №5
+Можно простые числа накапливать в массиве
 
 const MAX = 100;
 let number = 2;
